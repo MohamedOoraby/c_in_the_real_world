25041dfb29bc8690d0048c9b13dcbb59df84b79b
oraby <medo99oraby@gmail.com>
2023-09-23 14:42:44 +0300

    edting solve

--- exercises/ex1/main.c
+++ exercises/ex1/main.c
@@ -12,11 +12,12 @@ typedef struct {
 
 
 // finish the function (you may change the parametrs) 
-float point_get_distance(){
+float point_get_distance(point_t p1, point_t p2){
 
-    return PLACEHOLDER;
+    float res = sqrt(pow(p2.x - p1.x, 2)+pow(p2.y - p1.y, 2));
+
+    return res;
 }
-
 
 
 // find the distance between points (1, 6) and (9, 8)
@@ -25,7 +26,8 @@ int main(){
     point_t p2 = {9, 8};
 
 
-    printf("distance=%.2f", PLACEHOLDER);
+    printf("distance=%.2f\n", point_get_distance(p1, p2));
 
     return 0;
 }
+
